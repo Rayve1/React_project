b0bd59dd771799437c94101a9f830988ffa30c18
Rayve1 <rayve3432@gmail.com>
2023-05-19 13:14:03 +0400

    Create name button

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ function App() {
   return (
     <div className="App">
        <button url="#">
-
+          Статьи 
        </button>
        <button url="#">
-        
+          Тесты 
        </button>
        <button url="#">
-        
+          Игры
        </button>
     </div>
   );
